a19cd59a22b715bb58570d3a459e6035cd081ea3
Rohit <nathrohit48@gmail.com>
2025-06-15 20:41:10 +0530

    Different Thread methods.

--- src/main/java/net/javaguides/MyThread.java
+++ src/main/java/net/javaguides/MyThread.java
@@ -4,22 +4,37 @@ public class MyThread extends Thread{
 
     @Override
     public void run() {
-        System.out.println("RUNNING");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println(e);
-        }
+//        System.out.println("Thread is running....");
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            System.out.println(e);
+//        }
+
+//        for (int i = 1; i <= 5; i++){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+//            System.out.println(i);
+//        }
     }
 
     public static void main(String[] args) throws InterruptedException {
+//        MyThread t1 = new MyThread();
+//        System.out.println(t1.getState());
+//        t1.start();
+//        System.out.println(t1.getState());
+//        Thread.sleep(100);
+//        System.out.println(t1.getState());
+//        t1.join();
+//        System.out.println(t1.getState());
         MyThread t1 = new MyThread();
-        System.out.println(t1.getState());
         t1.start();
-        System.out.println(t1.getState());
-        Thread.sleep(100);
-        System.out.println(t1.getState());
         t1.join();
-        System.out.println(t1.getState());
+        System.out.println("Hello");
     }
 }
+
+
