22d87f9240eb0cd2c5c6f7a082da5d6c7039ffa8
Rohit <nathrohit48@gmail.com>
2025-06-15 20:03:00 +0530

    Created Thread.

---
+++ src/main/java/net/javaguides/World.java
@@ -0,0 +1,10 @@
+package net.javaguides;
+
+public class World extends Thread{
+    @Override
+    public void run() {
+        for(; ; ){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
