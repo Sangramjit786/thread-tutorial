1df7c86b1d9e7ec265806552057c36748108833b
Rohit <nathrohit48@gmail.com>
2025-06-15 20:32:31 +0530

    Getting Thread States.

--- src/main/java/net/javaguides/Test.java
+++ src/main/java/net/javaguides/Test.java
@@ -2,10 +2,17 @@ package net.javaguides;
 
 public class Test {
     public static void main(String[] args) {
-        World world = new World();
-        world.start();
+//        World world = new World();
+//        world.start();
+//        Thread t1 = new Thread(world);
+//        t1.start();
+        World t1 = new World(); //NEW
+        t1.start(); //RUNNABLE
+
+        System.out.println(Thread.currentThread().getName());
+
         for(; ; ){
-            System.out.println(Thread.currentThread().getName());
+            System.out.println("Hello");
         }
     }
 }
--- src/main/java/net/javaguides/World.java
+++ src/main/java/net/javaguides/World.java
@@ -1,10 +1,11 @@
 package net.javaguides;
 
 public class World extends Thread{
+//public class World implements Runnable{
     @Override
     public void run() {
         for(; ; ){
-            System.out.println(Thread.currentThread().getName());
+            System.out.println("World");
         }
     }
 }
