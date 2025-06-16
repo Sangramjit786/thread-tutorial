4e9e09b886a8be3ba3c1348dd3dcc4a35b6485e9
Rohit <nathrohit48@gmail.com>
2025-06-16 14:16:21 +0530

    Future implementation for ThreadPool, with Callable interface. But, in callable you can return anything.

--- src/main/java/net/javaguides/FutureImplementaion.java
+++ src/main/java/net/javaguides/FutureImplementaion.java
@@ -1,16 +1,15 @@
 package net.javaguides;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class FutureImplementaion {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Runnable runnable = () -> System.out.println("Hello");
-        Future<?> future = executorService.submit(runnable);
+//        Runnable runnable = () -> System.out.println("Hello");
+//        Future<?> future = executorService.submit(runnable);
 //        Future<?> future = executorService.submit(() -> System.out.println("Hello")); // runnable parameter
+        Callable<String> callable = () -> "Hello";
+        Future<?> future = executorService.submit(callable);
         System.out.println(future.get()); // blocking call ( null )
         if(future.isDone()){
             System.out.println("Task is done !");
